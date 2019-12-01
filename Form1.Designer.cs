b2f5e97d7552452aa958ab3122980fd780696e54
SimonaSadlekova <58397657+SimonaSadlekova@users.noreply.github.com>
2019-12-01 20:20:04 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Receive_Show_Save_Data
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tbValue = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cpb_com_port = new System.Windows.Forms.ComboBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnSaveData = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // tbValue
+            // 
+            this.tbValue.Location = new System.Drawing.Point(44, 62);
+            this.tbValue.Multiline = true;
+            this.tbValue.Name = "tbValue";
+            this.tbValue.Size = new System.Drawing.Size(251, 326);
+            this.tbValue.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Value:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(366, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Port:";
+            // 
+            // cpb_com_port
+            // 
+            this.cpb_com_port.FormattingEnabled = true;
+            this.cpb_com_port.Location = new System.Drawing.Point(369, 62);
+            this.cpb_com_port.Name = "cpb_com_port";
+            this.cpb_com_port.Size = new System.Drawing.Size(121, 24);
+            this.cpb_com_port.TabIndex = 3;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(360, 102);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 4;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(441, 102);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(379, 155);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(121, 48);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(379, 229);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(121, 48);
+            this.btnStop.TabIndex = 7;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnSaveData
+            // 
+            this.btnSaveData.Location = new System.Drawing.Point(379, 303);
+            this.btnSaveData.Name = "btnSaveData";
+            this.btnSaveData.Size = new System.Drawing.Size(121, 48);
+            this.btnSaveData.TabIndex = 8;
+            this.btnSaveData.Text = "Save Data";
+            this.btnSaveData.UseVisualStyleBackColor = true;
+            this.btnSaveData.Click += new System.EventHandler(this.btnSaveData_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 450);
+            this.Controls.Add(this.btnSaveData);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.cpb_com_port);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbValue);
+            this.Name = "Form1";
+            this.Text = "Měřič spotřeby";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbValue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cpb_com_port;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnSaveData;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Receive_Show_Save_Data
+{
+    public partial class Form1 : Form
+    {  
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cpb_com_port_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            cpb_com_port.Items.AddRange(ports);
+            cpb_com_port.SelectedIndex = 0;
+            btnClose.Enabled = false;
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            btnOpen.Enabled = false;
+            btnClose.Enabled = true;
+            try
+
+            {
+                serialPort1.PortName = cpb_com_port.Text;
+                serialPort1.Open();
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            serialPort1.BaudRate = 9600;
+            serialPort1.Parity = Parity.None;
+            serialPort1.DataBits = 8;
+            serialPort1.StopBits = StopBits.One;
+            serialPort1.DataReceived += SerialPort1_DataReceived;
+            try 
+            {
+                tbValue.Text = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Error");
+            }
+
+
+
+        }
+
+        private void SerialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string in_data_voltage = serialPort1.ReadLine();
+            string in_data_current = serialPort1.ReadLine();
+            string in_data = in_data_voltage + ";" + in_data_current;
+            tbValue.Text = in_data;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            try {
+
+                serialPort1.Close();
+            }
+            catch (Exception ex2)
+            {
+                MessageBox.Show(ex2.Message, "Error");
+            }
+
+
+        }
+
+        private void btnSaveData_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string pathFile = @"C:\Users\NTB\Desktop\DATA";
+                string fileName = "measure_data.txt";
+                System.IO.File.WriteAllText(pathFile + fileName, tbValue.Text);
+                MessageBox.Show("Your data has been saved to" + pathFile, "Save file.");
+            }
+
+            catch (Exception ex3)
+            {
+                MessageBox.Show(ex3.Message, "Error");
+            }
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort1.IsOpen)
+                serialPort1.Close();
+        }
+    }
+    }
+}
